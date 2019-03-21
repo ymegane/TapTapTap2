76696172e7859697cef6eb7e3c06d0fd1a0b698d
ymegane <crybaby.sumire+github@gmail.com>
2019-03-21 23:34:50 +0900

    :+1: Play sound effects

--- lib/widget/circles.dart
+++ lib/widget/circles.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:rxdart/rxdart.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
+import 'package:soundpool/soundpool.dart';
 
 import 'package:taptaptap2/bloc/circles_bloc.dart';
 import 'package:taptaptap2/bloc/circles_bloc_provider.dart';
@@ -13,9 +17,18 @@ class Circles extends StatefulWidget {
 
 class _CirclesState extends State<Circles> {
   PublishSubject<DragUpdateDetails> _moveEvents;
+  Soundpool _soundpool;
+  final List<int> _soundEffects = <int>[];
+
   @override
   void initState() {
     super.initState();
+    // load sound effects
+    _soundpool = Soundpool(streamType: StreamType.music, maxStreams: 5);
+
+    _loadEffectSounds(context).then((List<int> effects) {
+      _soundEffects.addAll(effects);
+    });
     final CirclesBloc bloc = CirclesBlocProvider.of(context);
     _moveEvents = PublishSubject<DragUpdateDetails>();
     _moveEvents
@@ -50,6 +63,7 @@ class _CirclesState extends State<Circles> {
   @override
   void dispose() {
     _moveEvents.close();
+    _soundpool.release();
     super.dispose();
   }
 
@@ -67,7 +81,7 @@ class _CirclesState extends State<Circles> {
     _addCircle(bloc, localOffset);
   }
 
-  void _addCircle(CirclesBloc bloc, Offset offset) {
+  Future<void> _addCircle(CirclesBloc bloc, Offset offset) async {
     const double circleRadius = Circle.CIRCLE_SIZE / 2;
 
     bloc.circleAddition.add(
@@ -78,5 +92,47 @@ class _CirclesState extends State<Circles> {
         bloc: bloc,
       ),
     );
+    await _playEffect();
+  }
+
+  Future<List<int>> _loadEffectSounds(BuildContext context) async {
+    final int sound1 = await rootBundle
+        .load('assets/ani_ge_chicken_koke03.mp3')
+        .then((ByteData soundData) {
+      return _soundpool.load(soundData);
+    });
+    final int sound2 = await rootBundle
+        .load('assets/ani_ge_dog_wan01.mp3')
+        .then((ByteData soundData) {
+      return _soundpool.load(soundData);
+    });
+    final int sound3 = await rootBundle
+        .load('assets/ani_ge_horse01.mp3')
+        .then((ByteData soundData) {
+      return _soundpool.load(soundData);
+    });
+    final int sound4 = await rootBundle
+        .load('assets/ani_ge_owl02.mp3')
+        .then((ByteData soundData) {
+      return _soundpool.load(soundData);
+    });
+    final int sound5 = await rootBundle
+        .load('assets/ani_ge_ushi02.mp3')
+        .then((ByteData soundData) {
+      return _soundpool.load(soundData);
+    });
+    return <int>[sound1, sound2, sound3, sound4, sound5];
+  }
+
+  final Random random = Random.secure();
+  Future<void> _playEffect() async {
+    if (_soundEffects.isEmpty) {
+      return;
+    }
+    final int soundId = _soundEffects[random.nextInt(_soundEffects.length)];
+    if (soundId == -1) {
+      return;
+    }
+    await _soundpool.play(soundId);
   }
 }
