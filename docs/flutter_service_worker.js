5e9eff554f1feba3fbfecb74fae38e9dfb54dead
ymegane <crybaby.sumire+github@gmail.com>
2020-05-08 00:43:17 +0900

    :memo: Upgrade web app assets

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1,10 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "ead4818b92a21b59deb2929bb08ed087",
-"/": "ead4818b92a21b59deb2929bb08ed087",
+  "favicon-16x16.png": "765276c7c36e893fdbe9137ccb1b5bc6",
+"favicon.ico": "6c0370d04fc24685915ea23e93844d41",
+"index.html": "ac04f926b00091dd9b859aaf50d4226e",
+"/": "ac04f926b00091dd9b859aaf50d4226e",
+"apple-touch-icon.png": "7fce73aadc7f2bdac2e88f68cbeeae4a",
 "main.dart.js.deps": "5b8b0e59bd0013d60f97349c99942a78",
 "main.dart.js": "9d62ace90ac58ce1e00d8d59c4804b43",
+"icons/android-chrome-192x192.png": "9e925489f2011fa23144b01bb7490615",
+"icons/android-chrome-512x512.png": "99b3a5e6770ea8642a86bd281cfcbede",
+"manifest.json": "87e1a60883e0d66aa3da2a65b5b8f20f",
 "assets/LICENSE": "5adbfec201afe2b462928fa60f26db09",
 "assets/AssetManifest.json": "7de30399d130d90f2c3489d8825f31f6",
 "assets/FontManifest.json": "e72e5fb5b510c86125a24438cde276f7",
@@ -16,7 +22,8 @@ const RESOURCES = {
 "assets/assets/ani_ge_chicken_koke03.mp3": "34fa5cecd871dcfa165912df46f7d6f3",
 "assets/assets/LuckiestGuy-Regular.ttf": "08010c3c508ed427652ebcc7f1926f38",
 "assets/assets/ani_ge_owl02.mp3": "210c9d3e64be1d4c83a61cc22c0f1d57",
-"assets/assets/ani_ge_dog_wan01.mp3": "15202b1031e2df8af57e4cf82ebf5684"
+"assets/assets/ani_ge_dog_wan01.mp3": "15202b1031e2df8af57e4cf82ebf5684",
+"favicon-32x32.png": "29812a6d9cc0796245d88763cf72e6f0"
 };
 
 self.addEventListener('activate', function (event) {
