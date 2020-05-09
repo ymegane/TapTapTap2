02f062b1d10a10c64a0315f7ba02b469821ffd84
ymegane <crybaby.sumire+github@gmail.com>
2020-05-09 14:58:22 +0900

    :+1: Switch dark mode in a new way

--- lib/widget/app.dart
+++ lib/widget/app.dart
@@ -7,28 +7,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (BuildContext context, Widget child) {
-        return Theme(
-          data: _getThemeData(context),
-          child: child,
-        );
-      },
+      title: 'TapTapTap',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (_) => LaunchScreen(),
         '/playground': (_) => PlaygroundScreen(),
       },
     );
-  }
-
-  ThemeData _getThemeData(BuildContext context) {
-    final bool darkMode = isDarkMode(context);
-    return ThemeData(brightness: darkMode ? Brightness.dark : Brightness.light);
-  }
-
-  static bool isDarkMode(BuildContext context) {
-    // for iOS13
-    final Brightness brightness = MediaQuery.of(context).platformBrightness;
-    return brightness == Brightness.dark;
   }
 }
